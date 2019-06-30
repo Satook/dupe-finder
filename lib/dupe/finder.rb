61c3a357addf4efde8b6e62f70b6b20ea1ab79fc
Andrew <me@satook.com>
2019-06-30 23:52:47 +1000

    Added file traversal

--- lib/dupe/finder.rb
+++ lib/dupe/finder.rb
@@ -1,9 +1,63 @@
-require "dupe/finder/version"
+require "find"
+require "digest"
 
 module Dupe
   module Finder
-    class Error < StandardError; end
-    # Your code goes here...
+
+    ##
+    # Returns false if we should ignore the directory or file
+    #
+    # Mainly, we ignore . files and directories.
+    #
+
+    def Finder.interesting_path?(path)
+      # we want to ignore dot files
+      File.basename(path)[0] != ?.
+    end
+
+    ##
+    # Recurses a directory yielding files that are interesting, to us
+    #
+    # If a block is given, it is called with each file path. Otherwise an
+    # enumerator is returned.
+    #
+
+    def Finder.interesting_files(path) # :yield: path
+      block_given? or return enum_for(__method__, path)
+
+      Find.find(path) do |path|
+        if !interesting_path?(path)
+          Find.prune
+        elsif FileTest.directory?(path)
+          next
+        else
+          yield path
+        end
+      end
+      nil
+    end
+
+    ##
+    # Calculates the SHA-256 digest of a file and returns it as a byte array
+    #
+
+    def file_digest(path)
+      Digest::SHA2.new().file(path).digest
+    end
+
+    #
+    # TODO: set up filters
+    # get sequence of file paths
+
+    #
+    # hash files
+    # store hashes
+    # let hash store sync
+    #
+    # iterate over (hash, [path]) pairs
+    # set hash to ignored
+    # print dupes
+    #
 
   end # module Dupe
 end # module Finder
--- spec/dupe/finder_spec.rb
+++ spec/dupe/finder_spec.rb
@@ -3,7 +3,31 @@ RSpec.describe Dupe::Finder do
     expect(Dupe::Finder::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(false)
+  it "filters files" do
+    Case = Struct.new(:path, :want)
+    cases = [
+      Case.new(".dontwant", false),
+      Case.new("/home/bob/pics/.dontwant", false),
+      Case.new("/home/bob/pics/dowant", true),
+      Case.new("/home/bob/pics/dowant/.other", false)
+    ]
+
+    cases.each do |c|
+      expect(Dupe::Finder::interesting_path? c.path).to eq(c.want)
+    end
+  end
+
+  it "finds files" do
+    base_dir = "test-files/dir"
+    files = Dupe::Finder::interesting_files(base_dir).collect { |x| x }
+    wants = ["dir2/file3", "dir2/file4", "file1", "file2"]
+    wants = wants.map { |x| base_dir + "/" + x }
+
+    # the order is deterministic because the find module sorts its output
+    expect(files).to eq(wants)
+  end
+
+  it "hashes files" do
+
   end
 end
