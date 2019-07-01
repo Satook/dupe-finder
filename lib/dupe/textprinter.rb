f19d0c487c9a812669970c76fbd59f3a108cff1f
Andrew <me@satook.com>
2019-07-01 22:40:36 +1000

    Printer now takes io dest as arg

--- lib/dupe/textprinter.rb
+++ lib/dupe/textprinter.rb
@@ -1,10 +1,27 @@
 
 module Dupe
 
+##
+# Prints out in a human readable format
+#
+
 class TextPrinter
+
+  ##
+  # Sets up the printer to write its output to `dest`
+  #
+
+  def initialize(dest)
+    @dest = dest
+  end
+
+  ##
+  # Print the file paths for a group of duplicate
+  #
+
   def print_dupes(paths)
-    puts "Duplicates found on paths:"
-    paths.each { |p| puts "\t#{p}"}
+    @dest.puts "Duplicates found on paths:"
+    @dest.puts paths.map { |p| "\t#{p}" }
   end
 
 end # class TextPrinter
--- spec/dupe/textprinter_spec.rb
+++ spec/dupe/textprinter_spec.rb
@@ -1,8 +1,12 @@
+require 'stringio'
+
 RSpec.describe Dupe::TextPrinter do
   it "prints as we'd expect" do
+    dest = StringIO.new
     paths = ["a", "b"]
     want = "Duplicates found on paths:\n\ta\n\tb\n"
+    Dupe::TextPrinter.new(dest).print_dupes paths
 
-    expect{subject.print_dupes paths}.to output(want).to_stdout
+    expect(dest.string).to eq(want)
   end
 end
