51730eb91e83b9b36993a585651462a480cde946
Andrew <me@satook.com>
2019-07-01 20:38:35 +1000

    Added additional hashstore test

--- spec/dupe/hashstore_spec.rb
+++ spec/dupe/hashstore_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe Dupe::HashStore do
     expect(@hs.each_pair.map { |k,v| k }).to eq([fakehash])
     expect(@hs.each_pair.map { |k,v| v }.flatten).to eq([fakepath])
   end
+
+  it "handles multiple paths with the same hash" do
+    fakehash, fakepath = 'notahash', '/home/some/path/to/file'
+    paths = 20.times.map {|x| "#{fakepath}#{x}"}
+
+    paths.each {|p| @hs.push(fakehash, p)}
+
+    all_pairs = @hs.each_pair.map {|k,v| [k,v] }
+    expect(all_pairs.count).to eq(1)
+    pair = all_pairs[0]
+    expect(pair[0]).to eq(fakehash)
+    expect(pair[1]).to eq(paths)
+  end
 end
