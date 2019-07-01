bcb31a3c8e6cf082fd7231bbc632831e5e9dbcfb
Andrew <me@satook.com>
2019-07-01 21:49:50 +1000

    Use subject instead of @hs

--- spec/dupe/hashstore_spec.rb
+++ spec/dupe/hashstore_spec.rb
@@ -1,25 +1,21 @@
 RSpec.describe Dupe::HashStore do
 
-  before(:example) do
-    @hs = Dupe::HashStore.new()
-  end
-
   it "can be passed hash/path pairs" do
     fakehash, fakepath = 'notahash', '/home/some/path/to/file'
 
-    @hs.push(fakehash, fakepath)
+    subject.push(fakehash, fakepath)
 
-    expect(@hs.each_pair.map { |k,v| k }).to eq([fakehash])
-    expect(@hs.each_pair.map { |k,v| v }.flatten).to eq([fakepath])
+    expect(subject.each_pair.map { |k,v| k }).to eq([fakehash])
+    expect(subject.each_pair.map { |k,v| v }.flatten).to eq([fakepath])
   end
 
   it "handles multiple paths with the same hash" do
     fakehash, fakepath = 'notahash', '/home/some/path/to/file'
     paths = 20.times.map {|x| "#{fakepath}#{x}"}
 
-    paths.each {|p| @hs.push(fakehash, p)}
+    paths.each {|p| subject.push(fakehash, p)}
 
-    all_pairs = @hs.each_pair.map {|k,v| [k,v] }
+    all_pairs = subject.each_pair.map {|k,v| [k,v] }
     expect(all_pairs.count).to eq(1)
     pair = all_pairs[0]
     expect(pair[0]).to eq(fakehash)
