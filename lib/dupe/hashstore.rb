2d8eae0a4593e451d6ff56527810f3748682a59a
Andrew <me@satook.com>
2019-07-01 21:48:42 +1000

    Added more doc comments

--- lib/dupe/hashstore.rb
+++ lib/dupe/hashstore.rb
@@ -9,13 +9,27 @@ module Dupe
 
   class HashStore
 
+    ##
+    # Starts off empty
+    #
+
     def initialize()
       @hashdict = Hash.new {|h,k| h[k] = []}
     end
 
+    ##
+    # Add a new hash/path pair to the store
+    #
+
     def push(hash, path)
       @hashdict[hash] << path
+
+      self
     end
+
+    ##
+    # Yields each hash, [path] pair that has been stored.
+    #
 
     def each_pair() # :yield: hash, paths
       block_given? or return enum_for(__method__)
@@ -23,6 +37,16 @@ module Dupe
       @hashdict.each_pair do |k, v|
         yield k, v
       end
+
+      self
+    end
+
+    ##
+    # Perform any necessary filesystem sync before we start reading
+    #
+
+    def sync()
+      self
     end
 
   end # class HashStore
