89f1c464de4ef7f56e2c0bd5047bdcdcd62ef490
Andrew <me@satook.com>
2019-07-01 20:06:24 +1000

    Fixed warning in test cases

--- spec/dupe/finder_spec.rb
+++ spec/dupe/finder_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Dupe::Finder do
   end
 
   it "filters files" do
-    Case = Struct.new(:path, :want)
+    FCase = Struct.new(:path, :want)
     cases = [
-      Case.new(".dontwant", false),
-      Case.new("/home/bob/pics/.dontwant", false),
-      Case.new("/home/bob/pics/dowant", true),
-      Case.new("/home/bob/pics/dowant/.other", false)
+      FCase.new(".dontwant", false),
+      FCase.new("/home/bob/pics/.dontwant", false),
+      FCase.new("/home/bob/pics/dowant", true),
+      FCase.new("/home/bob/pics/dowant/.other", false)
     ]
 
     cases.each do |c|
@@ -29,17 +29,16 @@ RSpec.describe Dupe::Finder do
   end
 
   it "hashes files" do
-    Case = Struct.new(:path, :hexdigest)
+    HCase = Struct.new(:path, :hexdigest)
     base_path = "test-files/digest/"
     test_files = [
-      Case.new("test1.txt", "3e62b4bd08faa9bce87955c15fd8e6152d12cfca6a3961e93ace96575156c8bf"),
-      Case.new("test2.txt", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
-    ].map { |x| Case.new(base_path + x.path, x.hexdigest) }
+      HCase.new("test1.txt", "3e62b4bd08faa9bce87955c15fd8e6152d12cfca6a3961e93ace96575156c8bf"),
+      HCase.new("test2.txt", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
+    ].map { |x| HCase.new(base_path + x.path, x.hexdigest) }
 
     test_files.each do |x|
       got = Dupe::Finder.file_digest(x.path).unpack('H*').first
       expect(got).to eq(x.hexdigest)
     end
-
   end
 end
